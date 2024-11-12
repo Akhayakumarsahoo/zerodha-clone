a7aa8cc8f28792bdb6427cfcd55e21518e682766
Akhaya <akhayakumarsahoo06@gmail.com>
2024-11-12 18:05:57 +0530

    updation04

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3002",
+      "/api": "https://zerodha-backend-21fn.onrender.com",
     },
   },
   plugins: [react()],
